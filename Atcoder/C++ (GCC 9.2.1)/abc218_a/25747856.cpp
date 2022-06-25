b34809b3b185f0f206953f8f2db296e6f257ebd2
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 08:58:22 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc218_a/25747856.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define INGZzz  	cin.tie(0);cin.sync_with_stdio(0);
+const int N = 1e5;
+
+int main()
+{
+    INGZzz
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if(s[n-1] == 'o') cout << "Yes\n";
+    else cout << "No\n";
+   
+    return 0;
+}
