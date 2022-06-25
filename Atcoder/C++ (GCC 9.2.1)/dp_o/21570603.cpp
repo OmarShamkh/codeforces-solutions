f6e21f4b4c872a4673a01fa67bf47b0518e29919
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 08:58:24 +0200

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/dp_o/21570603.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define INGZzz  	cin.tie(0);cin.sync_with_stdio(0);
+const int N = 1e5;
+
+int main()
+{
+    INGZzz
+    int n;
+    cin >> n;
+    cout << n*(n+1) /2 <<"\n";
+    
+    return 0;
+}
