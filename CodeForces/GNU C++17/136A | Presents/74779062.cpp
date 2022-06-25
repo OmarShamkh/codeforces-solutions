c3085f45116f08610c4775d1a955109bb444d97f
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:13:23 +0200

    Committing files

---
+++ CodeForces/GNU C++17/136A | Presents/74779062.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int a[101];
+int main()
+{
+ 
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        int x;
+        cin >> x;
+        a[x] = i;
+ 
+    }
+    for(int i=1; i<=n; i++) cout << a[i] << " ";
+    return 0;
+}
