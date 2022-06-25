98b9a99745546d1fc5ba8a75406e8ff2b0494e2c
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:06:25 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1475B | New Year's Number/108155483.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+typedef long long ll;
+#define loop(n)          for(int i = 0; i < (n); i++)
+#define lp(x, s, e)      for(int x = (s); x < (e); x++)
+#define lpe(x, s, e)     for(int x = (s); x <= (e); x++)
+const int N = 1e5;
+ 
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << ((n%2020)*2021 <= n ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
