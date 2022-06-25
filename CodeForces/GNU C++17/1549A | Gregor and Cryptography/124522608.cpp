361e473a6b528d43f8e3965824600677ce8938bf
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:03:00 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1549A | Gregor and Cryptography/124522608.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef long double ld;
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int p;
+        cin >> p;
+        cout << 2 << " " << p-1 << "\n";
+    }
+    
+    return 0;
+}
