d61bde51fd3b5627a292f5df40087ca48e977bc3
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:01:31 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1604A | Era/135252370.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+#define loop(n)          for(int i = 0; i < (n); i++)
+#define lp(x, s, e)      for(int x = (s); x < (e); x++)
+#define lpe(x, s, e)     for(int x = (s); x <= (e); x++)
+#define len(s)   (int)s.length()
+#define sz(v)    (int)v.size()
+#define all(a)   a.begin(),a.end()
+const int N = 105, OO = 0x3f3f3f3f, mod = 1e9 + 7;
+int a[N];
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        for(int i=0; i<n; i++) cin >> a[i];
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            ans = max(ans , a[i-1]-i);
+        }
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
