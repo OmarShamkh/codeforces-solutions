38087edde54d4e696d6b815514cce052ed8d4276
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:14:26 +0200

    Committing files

---
+++ CodeForces/GNU C++17/952A | Quirky Quantifiers/52240236.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    cout.tie(0);
+ 
+    int n;
+    cin>>n;
+ 
+    if(n % 2 == 0) cout<<0;
+    else cout<<1;
+ 
+    return 0;
+ 
+}
