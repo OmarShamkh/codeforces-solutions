9ff6d081f1903ec71a4e0ba5a43b2665689f0f9b
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:15:05 +0200

    Committing files

---
+++ CodeForces/GNU C++14/263A | Beautiful Matrix/49532907.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int a[5][5];
+    int c=0;
+    for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+        cin>>a[i][j];
+        if(a[i][j]==1){
+            c+=abs(i-2)+abs(j-2);
+        }
+            }
+    }
+    cout<<c;
+ 
+    return 0;
+}
