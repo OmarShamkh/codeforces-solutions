10d5c8c816a2ee8b57b42a8195a42aeb144d1438
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:15:50 +0200

    Committing files

---
+++ CodeForces/Java 8/281A | Word Capitalization/46906128.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+ 
+public class Exmples {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        String word = in.next();
+        
+        if (Character.isUpperCase(word.charAt(0))) {
+            System.out.println(word);
+        }
+        if (Character.isLowerCase(word.charAt(0))) {
+            System.out.println(Character.toUpperCase(word.charAt(0)) + word.substring(1));
+        }
+        
+    }
+    
+}
