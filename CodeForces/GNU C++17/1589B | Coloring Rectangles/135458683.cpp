11652754a843326b0c7635c1826010d2010826b0
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:01:02 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1589B | Coloring Rectangles/135458683.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+#define loop(n)          for(int i = 0; i < (n); i++)
+#define lp(x, s, e)      for(int x = (s); x < (e); x++)
+#define lpe(x, s, e)     for(int x = (s); x <= (e); x++)
+#define len(s)   (int)s.length()
+#define sz(v)    (int)v.size()
+#define all(a)   a.begin(),a.end()
+const int N = 1e5, OO = 0x3f3f3f3f, mod = 1e9 + 7;
+ 
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+        cout << n*m / 3 + ((n*m %3)!=0) << "\n"; 
+    }
+    
+    return 0;
+}
