074120c22ffd51dfd4ead695623fbc6ff564f79c
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:14:11 +0200

    Committing files

---
+++ CodeForces/GNU C++17/770A | New Password/63682766.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    string ans;
+    for(int i =0; i<n; i++) ans+='a'+(i%k);
+    cout<<ans;
+    return 0;
+}
