0151e7224e74d3e42a2c019bd814017bcdd4073a
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2022-06-25 09:02:14 +0200

    Committing files

---
+++ CodeForces/GNU C++17/1360A | Minimal Square/130969312.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+using namespace std;
+typedef long long ll;
+#define INGZzz   cin.tie(0);cin.sync_with_stdio(0);
+const int N = 1e5;
+ 
+int main()
+{
+    INGZzz
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        int ans = min( max(2*a, b), max(a, 2*b));
+        cout << ans*ans << "\n";
+    }
+    
+    return 0;
+}
